d13bcdf0a4bfdb8a5c9833e15e78766ae348f500
mikumaraditya <mikumaraditya@gmail.com>
2026-05-18 09:30:00 +0530

    Add Socket.io for real-time group updates

---
+++ backend/src/socket.js
@@ -0,0 +1,41 @@
+import { Server } from "socket.io";
+
+let io;
+
+const initSocket = (server) => {
+  io = new Server(server, {
+    cors: {
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    // user joins a group room
+    socket.on("join_group", (groupId) => {
+      socket.join(groupId);
+      console.log(`User ${socket.id} joined group ${groupId}`);
+    });
+
+    // user leaves a group room
+    socket.on("leave_group", (groupId) => {
+      socket.leave(groupId);
+      console.log(`User ${socket.id} left group ${groupId}`);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected:", socket.id);
+    });
+  });
+
+  return io;
+};
+
+const getIO = () => {
+  if (!io) throw new Error("Socket not initialized");
+  return io;
+};
+
+export { initSocket, getIO };
