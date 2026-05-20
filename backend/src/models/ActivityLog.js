3f0e697f5929c131e50550d8dabd24c5209aec0b
mikumaraditya <mikumaraditya@gmail.com>
2026-05-20 09:00:00 +0530

    Add activity log model for group expense tracking

---
+++ backend/src/models/ActivityLog.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose'
+
+const activityLogSchema = new mongoose.Schema(
+  {
+    group: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Group',
+      required: true,
+      index: true,
+    },
+    actor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: [
+        'expense_added',
+        'expense_deleted',
+        'settlement_requested',
+        'settlement_confirmed',
+        'settlement_disputed',
+        'evidence_submitted',
+        'dispute_resolved',
+        'member_added',
+        'member_removed',
+        'member_left',
+        'group_created',
+      ],
+    },
+    meta: {
+      type: mongoose.Schema.Types.Mixed,
+      default: {},
+    },
+  },
+  { timestamps: true }
+)
+
+export default mongoose.model('ActivityLog', activityLogSchema)
